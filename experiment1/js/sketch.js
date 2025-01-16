059ce1634a96f8569d6b521788cca51f172a365d
JackWilhelm <jgwilhel@ucsc.edu>
2025-01-15 19:26:35 -0800

    Added assets and have distance impact image rotation

--- experiment1/js/sketch.js
+++ experiment1/js/sketch.js
@@ -31,7 +31,33 @@ function resizeScreen() {
   centerVert = canvasContainer.height() / 2; // Adjusted for drawing logic
   console.log("Resizing...");
   resizeCanvas(canvasContainer.width(), canvasContainer.height());
-  // redrawCanvas(); // Redraw everything based on new size
+  //redrawCanvas(); // Redraw everything based on new size
+}
+
+'use strict';
+
+var tileCount = 10;
+
+var tileWidth;
+var tileHeight;
+var shapeSize = 50;
+var newShapeSize = shapeSize;
+var shapeAngle = 0;
+var maxDist;
+var currentShape;
+var shapes;
+
+var sizeMode = 0;
+
+function preload() {
+  shapes = [];
+  shapes.push(loadImage('data/module_1.svg'));
+  shapes.push(loadImage('data/module_2.svg'));
+  shapes.push(loadImage('data/module_3.svg'));
+  shapes.push(loadImage('data/module_4.svg'));
+  shapes.push(loadImage('data/module_5.svg'));
+  shapes.push(loadImage('data/module_6.svg'));
+  shapes.push(loadImage('data/module_7.svg'));
 }
 
 // setup() function is called once when the program starts
@@ -49,29 +75,71 @@ function setup() {
     resizeScreen();
   });
   resizeScreen();
+  imageMode(CENTER);
+  // set the current shape to the first in the array
+  currentShape = shapes[0];
+  tileWidth = 83.2;
+  tileHeight = 60;
+  maxDist = sqrt(pow(width, 2) + pow(height, 2));
 }
 
 // draw() function is called repeatedly, it's the main animation loop
 function draw() {
-  background(220);    
-  // call a method on the instance
-  myInstance.myMethod();
+  clear();
 
-  // Set up rotation for the rectangle
-  push(); // Save the current drawing context
-  translate(centerHorz, centerVert); // Move the origin to the rectangle's center
-  rotate(frameCount / 100.0); // Rotate by frameCount to animate the rotation
-  fill(234, 31, 81);
-  noStroke();
-  rect(-125, -125, 250, 250); // Draw the rectangle centered on the new origin
-  pop(); // Restore the original drawing context
+  for (var gridY = 0; gridY < tileCount; gridY++) {
+    for (var gridX = 0; gridX < tileCount; gridX++) {
 
-  // The text is not affected by the translate and rotate
-  fill(255);
-  textStyle(BOLD);
-  textSize(140);
-  text("p5*", centerHorz - 105, centerVert + 40);
+      var posX = tileWidth * gridX + tileWidth / 2;
+      var posY = tileHeight * gridY + tileWidth / 2;
+
+      // calculate angle between mouse position and actual position of the shape
+      var e = dist(mouseX, mouseY, posX, posY)/200;
+      var angle = lerp(
+        atan2(mouseY - posY, mouseX - posX) + (180 * (PI / 180)), 
+        atan2(mouseY - posY, mouseX - posX) + (0 * (PI / 180)), 
+        max(min(e*e, 1),0));
+
+      if (sizeMode == 0) newShapeSize = shapeSize;
+      if (sizeMode == 1) newShapeSize = shapeSize * 1.5 - map(dist(mouseX, mouseY, posX, posY), 0, 500, 5, shapeSize);
+      if (sizeMode == 2) newShapeSize = map(dist(mouseX, mouseY, posX, posY), 0, 500, 5, shapeSize);
+
+      push();
+      translate(posX, posY);
+      rotate(angle);
+      noStroke();
+      image(currentShape, 0, 0, newShapeSize, newShapeSize);
+      pop();
+    }
+  }
 }
+
+function keyReleased() {
+  if (key == 's' || key == 'S') saveCanvas(gd.timestamp(), 'png');
+  if (key == 'd' || key == 'D') sizeMode = (sizeMode + 1) % 3;
+  if (key == 'g' || key == 'G') {
+    tileCount += 5;
+    if (tileCount > 20) {
+      tileCount = 10;
+    }
+    tileWidth = width / tileCount;
+    tileHeight = height / tileCount;
+  }
+
+  if (key == '1') currentShape = shapes[0];
+  if (key == '2') currentShape = shapes[1];
+  if (key == '3') currentShape = shapes[2];
+  if (key == '4') currentShape = shapes[3];
+  if (key == '5') currentShape = shapes[4];
+  if (key == '6') currentShape = shapes[5];
+  if (key == '7') currentShape = shapes[6];
+
+  if (keyCode == UP_ARROW) shapeSize += 5;
+  if (keyCode == DOWN_ARROW) shapeSize = max(shapeSize - 5, 5);
+  if (keyCode == LEFT_ARROW) shapeAngle += 5;
+  if (keyCode == RIGHT_ARROW) shapeAngle -= 5;
+}
+
 
 // mousePressed() function is called once after every time a mouse button is pressed
 function mousePressed() {
