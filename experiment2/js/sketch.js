016555e0ea545e09168cb8854fdf4c6819810783
JackWilhelm <jgwilhel@ucsc.edu>
2025-01-22 10:08:38 -0800

    Experiment 3 start - simple Random Walk

--- experiment2/js/sketch.js
+++ experiment2/js/sketch.js
@@ -14,6 +14,7 @@ const VALUE2 = 2;
 let myInstance;
 let canvasContainer;
 var centerHorz, centerVert;
+let x,y;
 
 class MyClass {
     constructor(param1, param2) {
@@ -23,6 +24,14 @@ class MyClass {
 
     myMethod() {
         // code to run when method is called
+        point(x, y);
+        let xOld = x;
+        let yOld = y;
+        x += random(-5, 5);
+        y += random(-5, 5);
+        x = constrain(x, 0, width);
+        y = constrain(y, 0, height);
+        line(xOld, yOld, x, y);
     }
 }
 
@@ -49,28 +58,17 @@ function setup() {
     resizeScreen();
   });
   resizeScreen();
+  background(0);
+  stroke(255);
+  x = width / 2;
+  y = height / 2;
 }
 
 // draw() function is called repeatedly, it's the main animation loop
 function draw() {
-  background(220);    
+  //background(220);    
   // call a method on the instance
   myInstance.myMethod();
-
-  // Set up rotation for the rectangle
-  push(); // Save the current drawing context
-  translate(centerHorz, centerVert); // Move the origin to the rectangle's center
-  rotate(frameCount / 100.0); // Rotate by frameCount to animate the rotation
-  fill(234, 31, 81);
-  noStroke();
-  rect(-125, -125, 250, 250); // Draw the rectangle centered on the new origin
-  pop(); // Restore the original drawing context
-
-  // The text is not affected by the translate and rotate
-  fill(255);
-  textStyle(BOLD);
-  textSize(140);
-  text("p5*", centerHorz - 105, centerVert + 40);
 }
 
 // mousePressed() function is called once after every time a mouse button is pressed
