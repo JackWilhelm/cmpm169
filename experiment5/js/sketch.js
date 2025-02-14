e7bbd490edd1132b297b4f69e1c456dd92a98c29
JackWilhelm <jgwilhel@ucsc.edu>
2025-02-13 17:07:12 -0800

    Copied over data and code

--- experiment5/js/sketch.js
+++ experiment5/js/sketch.js
@@ -17,6 +17,38 @@ class MyClass {
     }
 }
 
+'use strict';
+
+var textTyped = '';
+var font;
+
+var shapeSpace;
+var shapeSpace2;
+var shapePeriod;
+var shapeComma;
+var shapeQuestionmark;
+var shapeExclamationmark;
+var shapeReturn;
+
+var centerX;
+var centerY;
+var offsetX;
+var offsetY;
+var zoom;
+
+var actRandomSeed;
+
+function preload() {
+  font = loadFont('data/miso-bold.ttf');
+  shapeSpace = loadImage('data/space.svg');
+  shapeSpace2 = loadImage('data/space2.svg');
+  shapePeriod = loadImage('data/period.svg');
+  shapeComma = loadImage('data/comma.svg');
+  shapeExclamationmark = loadImage('data/exclamationmark.svg');
+  shapeQuestionmark = loadImage('data/questionmark.svg');
+  shapeReturn = loadImage('data/return.svg');
+}
+
 function resizeScreen() {
   centerHorz = canvasContainer.width() / 2; // Adjusted for drawing logic
   centerVert = canvasContainer.height() / 2; // Adjusted for drawing logic
@@ -40,31 +72,138 @@ function setup() {
     resizeScreen();
   });
   resizeScreen();
+  textTyped += 'Ich bin der Musikant mit Taschenrechner in der Hand!\n\n';
+  textTyped += 'Ich addiere\n';
+  textTyped += 'Und subtrahiere, \n\n';
+  textTyped += 'Kontrolliere\nUnd komponiere\nUnd wenn ich diese Taste drück,\nSpielt er ein kleines Musikstück?\n\n';
+
+  textTyped += 'Ich bin der Musikant mit Taschenrechner in der Hand!\n\n';
+  textTyped += 'Ich addiere\n';
+  textTyped += 'Und subtrahiere, \n\n';
+  textTyped += 'Kontrolliere\nUnd komponiere\nUnd wenn ich diese Taste drück,\nSpielt er ein kleines Musikstück?\n\n';
+
+  centerX = width / 2;
+  centerY = height / 2;
+  offsetX = 0;
+  offsetY = 0;
+  zoom = 0.75;
+
+  actRandomSeed = 6;
+
+  cursor(HAND);
+  textFont(font, 25);
+  textAlign(LEFT, BASELINE);
+  noStroke();
+  fill(0);
 }
 
 // draw() function is called repeatedly, it's the main animation loop
 function draw() {
-  background(220);    
-  // call a method on the instance
-  myInstance.myMethod();
+  background(255);
 
-  // Set up rotation for the rectangle
-  push(); // Save the current drawing context
-  translate(centerHorz, centerVert); // Move the origin to the rectangle's center
-  rotate(frameCount / 100.0); // Rotate by frameCount to animate the rotation
-  fill(234, 31, 81);
-  noStroke();
-  rect(-125, -125, 250, 250); // Draw the rectangle centered on the new origin
-  pop(); // Restore the original drawing context
+  if (mouseIsPressed && mouseButton == LEFT) {
+    centerX = mouseX - offsetX;
+    centerY = mouseY - offsetY;
+  }
 
-  // The text is not affected by the translate and rotate
-  fill(255);
-  textStyle(BOLD);
-  textSize(140);
-  text("p5*", centerHorz - 105, centerVert + 40);
+  // allways produce the same sequence of random numbers
+  randomSeed(actRandomSeed);
+
+  translate(centerX, centerY);
+  scale(zoom);
+
+  for (var i = 0; i < textTyped.length; i++) {
+    var letter = textTyped.charAt(i);
+    var letterWidth = textWidth(letter);
+
+    // ------ letter rule table ------
+    switch (letter) {
+    case ' ': // space
+      // 50% left, 50% right
+      var dir = floor(random(0, 2));
+      if (dir == 0) {
+        image(shapeSpace, 1, -15);
+        translate(4, 1);
+        rotate(QUARTER_PI);
+      }
+      if (dir == 1) {
+        image(shapeSpace2, 1, -15);
+        translate(14, -5);
+        rotate(-QUARTER_PI);
+      }
+      break;
+
+    case ',':
+      image(shapeComma, 1, -15);
+      translate(35, 15);
+      rotate(QUARTER_PI);
+      break;
+
+    case '.':
+      image(shapePeriod, 1, -55);
+      translate(56, -56);
+      rotate(-HALF_PI);
+      break;
+
+    case '!':
+      image(shapeExclamationmark, 1, -27);
+      translate(42.5, -17.5);
+      rotate(-QUARTER_PI);
+      break;
+
+    case '?':
+      image(shapeQuestionmark, 1, -27);
+      translate(42.5, -17.5);
+      rotate(-QUARTER_PI);
+      break;
+
+    case '\n': // return
+      image(shapeReturn, 1, -15);
+      translate(1, 10);
+      rotate(PI);
+      break;
+
+    default: // all others
+      text(letter, 0, 0);
+      translate(letterWidth, 0);
+    }
+  }
+
+  // blink cursor after text
+  if (frameCount / 6 % 2 == 0) rect(0, 0, 15, 2);
 }
 
 // mousePressed() function is called once after every time a mouse button is pressed
 function mousePressed() {
     // code to run when mouse is pressed
+  offsetX = mouseX - centerX;
+  offsetY = mouseY - centerY;
+}
+
+function keyPressed() {
+  switch (keyCode) {
+  case DELETE:
+  case BACKSPACE:
+    textTyped = textTyped.substring(0, max(0, textTyped.length - 1));
+    print(textTyped);
+    break;
+  case ENTER:
+  case RETURN:
+    // enable linebreaks
+    textTyped += '\n';
+    break;
+  case UP_ARROW:
+    zoom += 0.05;
+    break;
+  case DOWN_ARROW:
+    zoom -= 0.05;
+    break;
+  }
+}
+
+function keyTyped() {
+  if (keyCode >= 32) {
+    textTyped += key;
+    print(textTyped);
+  }
 }
